c30dc949ab17f171bff768bf08ddee5362b18498
sunchangpeng <sunchangpeng@meituan.com>
2019-04-03 19:05:57 +0800

    add substitute logger test

--- src/main/java/org/slf4j/impl/StaticLoggerBinder.java
+++ src/main/java/org/slf4j/impl/StaticLoggerBinder.java
@@ -1,12 +1,17 @@
 package org.slf4j.impl;
 
 import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.spi.LoggerFactoryBinder;
 
 public class StaticLoggerBinder implements LoggerFactoryBinder {
+    private static final Logger log = LoggerFactory.getLogger(StaticLoggerBinder.class);
+
     private static final StaticLoggerBinder SINGLETON = new StaticLoggerBinder();
 
     public static final StaticLoggerBinder getSingleton() {
+        log.info("substitute logging calls");
         return SINGLETON;
     }
 
